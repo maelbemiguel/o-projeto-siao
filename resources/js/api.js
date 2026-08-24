f6f1365e08e22d81f48b8e274f12afff82b11e99
Miguel Maelbe <miguelmaelbetb05@gmail.com>
2026-08-24 20:49:33 -0300

    comentário

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({
+const api = axios.create({ //faz a conversão do form em um json para ser enviado, como a base é /api, qualquer endereço vem /api antes
     baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
@@ -9,7 +9,7 @@ const api = axios.create({
 })
 
 // Injeta o token Bearer em todas as requisições
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {  //intercepta a request antes de ela sair do navegador, busca o o toekn no localstore do navegador do usuario e colocar no header `Bearer ${token}`
     const token = localStorage.getItem('token')
     if (token) config.headers.Authorization = `Bearer ${token}`
     return config
